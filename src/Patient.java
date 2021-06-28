b123f4c17dd8775073e4da29a42d9f8246fe0a82
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-28 16:29:08 -0500

    set the getters and setters for patient

--- src/Patient.java
+++ src/Patient.java
@@ -14,4 +14,78 @@ public class Patient {
         this.name = name;
         this.email = email;
     }
+
+    //setters
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+    public void setHeight(double height){
+        this.height = height;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    //geters
+    public String getWeight(){
+        return this.weight + "kg";
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Patient.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() > 8)
+            System.out.println("The number must be a maximum of 8 numbers");
+        else
+            this.phoneNumber = phoneNumber;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getHeight() {
+        return this.height + "mts";
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
 }
