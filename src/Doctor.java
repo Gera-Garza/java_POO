376fc2d1e6cd002c1c348df543128ae4bf07946f
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-28 12:45:59 -0500

    Creating of package, and use of static

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,11 +1,29 @@
+import javax.print.Doc;
+
 public class Doctor {
-    int id;
+    //Atributes
+    static int id = 0; //autoincrement
     String name;
     String speciality;
+
+
+    //Constructor
+    Doctor(){
+        System.out.println("Creating the object Doctor");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("The doctor's name is: "+ name);
+    }
 
     //Methods
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Rodriguez";
-        myDoctor.showName();
+import static ui.UIMenu.*;
 
-    }
-}
+public class Main {
+
+    public static void main(String[] args) {
+        showMenu();
+
+    }//end of main
+
+}//end of Main class
